b49233a34b1c0c071fc91dbaab1332f737298d76
Olga Shaban <117861745+olga-shaban@users.noreply.github.com>
2023-04-17 22:48:54 +0300

    feat: update transform array

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,9 +13,48 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function transform(arr) {
+  if (!arr instanceof Array) {
+    throw Error('Not an array');
+  }
+
+  if (!Array.isArray(arr)) {
+    throw Error("'arr' parameter must be an instance of the Array!");
+  }
+  let arr2 = [];
+
+  for (let i = 0; i < arr.length; i++) {
+
+    switch (arr[i]) {
+      case '--discard-next':
+        if (arr[i + 2] === '--double-prev' || arr[i + 2] === '--discard-prev') {
+          i++;
+        }
+        i++;
+        break;
+
+      case '--discard-prev':
+        arr2.pop();
+        break;
+
+      case '--double-next':
+        if (i < arr.length - 1) {
+          arr2.push(arr[i + 1]);
+          arr2.push(arr[i + 1]);
+        }
+        i++;
+        break;
+
+      case '--double-prev':
+        if (i !== 0) {
+          arr2.push(arr[i - 1]);
+        }
+        break;
+      default: arr2.push(arr[i]);
+    }
+  }
+
+  return arr2;
 }
 
 module.exports = {
